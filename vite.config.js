256b4a75620ac3faf918f922949eada31e8ab376
fabricio-romero-126190 <kevinvarsoviat007@gmail.com>
2026-07-03 14:58:54 -0300

    liderKevinRomero

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,4 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
 export default defineConfig({
   plugins: [react()],
-  base: 'https://fabricioromero126190.github.io/examenviernes',
+  base: '/examenviernes/',
 })
